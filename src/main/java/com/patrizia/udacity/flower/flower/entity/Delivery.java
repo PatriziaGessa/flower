2f782d8dd67a11ef75ba204caf94e290bc8b7ae3
PatriziaGessa <patriziagessa@gmail.com>
2021-12-31 14:05:03 +0100

    Initial commit

---
+++ src/main/java/com/patrizia/udacity/flower/flower/entity/Delivery.java
@@ -0,0 +1,65 @@
+package com.patrizia.udacity.flower.flower.entity;
+
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Delivery {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized
+    private String name;
+    @Column(name = "address_full", length = 500)
+    private String address;
+    private LocalDateTime deliveryTime; //Includes both date and time - simpler than having two separate fields
+    @Type(type = "yes_no")
+    private Boolean competed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(LocalDateTime deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public Boolean getCompeted() {
+        return competed;
+    }
+
+    public void setCompeted(Boolean competed) {
+        this.competed = competed;
+    }
+}
